09a9bbf352c18c7c1d0a8b1d3c2aa73c43273d48
Philipp Schiessler <philipp.schiessler@mail.de>
2020-11-11 16:06:12 +0100

    created all

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+# get 'restaurants', to: 'restaurants#index'
+# get 'restaurants/new', to: 'restaurants#new'
+# post 'restaurants', to: 'restaurants#create'
+# get 'restaurants/:id', to: 'restaurants#show'
+# get  'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+# post 'restaurants/:restaurant_id/reviews', to: 'reviews#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'clearing data'
+Restaurant.destroy_all
+puts 'creating restaurants'
+Restaurant.create!(name:'Pizzariaaaa', address:'Foodstreet 4', phone_number:'089/3345664', category:'italian')
+Restaurant.create!(name:'Ni Hao', address:'Deliciousstreet 12', phone_number:'089/5553343', category:'chinese')
+Restaurant.create!(name:'Yoko Ono', address:'Sushistreet 77', phone_number:'555-SUSHI', category:'japanese')
+Restaurant.create!(name:'Oh la la', address:'Yam-Yam-Street 124', phone_number:'+55-4434-556', category:'french')
+Restaurant.create!(name:'Atomium', address:'Brusselstreet 1', phone_number:'887876656566655', category:'belgian')
+puts 'finished!'
